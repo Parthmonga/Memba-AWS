1eeeab3af5a8e5f9d31289692d878b196d79ade0
jlchereau <jlchereau@msn.com>
2015-06-03 14:38:57 +0100

    Added check of key pairs

---
+++ commands/check-ec2-key-pair.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+source ~/.bashrc
+
+echo '------------------------------------------------------------'
+echo '>>> key pair check'
+
+if [ $# -lt 1 ]
+then
+    echo '>>> missig arguments'
+    exit
+fi
+
+KEY_NAME=$1
+
+COUNT=$(aws ec2 describe-key-pairs --query "length(KeyPairs)" --output text)
+if [ $COUNT -gt 1 ]
+then
+    echo '>>> WARNING: You have' $COUNT 'key pairs'
+fi
+
+KEY_FOUND=$(aws ec2 describe-key-pairs --query "KeyPairs[?KeyName == '$KEY_NAME'].KeyName" --output text)
+if [ -z KEY_FOUND ]
+then
+     echo '>>>' $KEY_NAME 'not found'
+else
+     echo '>>>' $KEY_NAME 'found'
+    # Maybe we should consider creating a new key pair in this case
+    # Note that renewing key pairs with each installation improves security
+fi
