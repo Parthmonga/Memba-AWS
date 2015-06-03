9b07d10fdd1fe4f75624efaba089522e95269b72
jlchereau <jlchereau@msn.com>
2015-06-03 14:44:46 +0100

    Parameterized ports

--- commands/create-ec2-load-balancer.sh
+++ commands/create-ec2-load-balancer.sh
@@ -4,21 +4,26 @@ source ~/.bashrc
 echo '------------------------------------------------------------'
 echo '>>> load balancer creation'
 
-if [ $# -lt 3 ]
+if [ $# -lt 8 ]
 then
     echo '>>> missig arguments'
     exit
 fi
 
-VPC_CIDR=$1
-SG_NAME=$2
-ELB_NAME=$3
+VPC_CIDR=$1         #"10.1.0.0/16"
+SG_ELB_NAME=$2      #"productionELBSecurityGroup"
+ELB_NAME=$3         #"productionLoadBalancer"
+ELB_HTTP_PORT=$4    #"80"
+ELB_HTTPS_PORT=$5   #"443"
+EC2_HTTP_PORT=$6    #"80"
+EC2_HTTPS_PORT=$7   #"443"
+CERT_NAME=$8        #"kidojuSSLCertificate"
 
-# http://docs.aws.amazon.com/cli/latest/reference/elb/describe-load-balancers.html
-# aws elb describe-load-balancers
-
-ELB_HTTP_LISTENER="Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80"
-ELB_HTTPS_LISTENER="Protocol=HTTPS,LoadBalancerPort=443,InstanceProtocol=HTTP,InstancePort=443"
+# https://www.memba.com
+ELB_HTTP_LISTENER="Protocol=HTTP,LoadBalancerPort="$ELB_HTTP_PORT",InstanceProtocol=HTTP,InstancePort="$EC2_HTTP_PORT
+# https://www.kidoju.com
+CERT_ID=$(aws iam get-server-certificate --server-certificate-name "kidojuSSLCertificate" --query "ServerCertificate.ServerCertificateMetadata.Arn" --output text)
+ELB_HTTPS_LISTENER="Protocol=HTTPS,LoadBalancerPort="$ELB_HTTPS_PORT",InstanceProtocol=HTTP,InstancePort="$EC2_HTTPS_PORT",SSLCertificateId="$CERT_ID
 
 # http://docs.aws.amazon.com/cli/latest/reference/ec2/describe-vpcs.html
 VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?CidrBlock=='$VPC_CIDR'].[VpcId][0][0]" --output text)
@@ -34,11 +39,11 @@ else
     echo '>>>' $VPC_SUBNETS 'subnets found'
 
     # http://docs.aws.amazon.com/cli/latest/reference/ec2/describe-security-groups.html
-    SG_ID=$(aws ec2 describe-security-groups --query "SecurityGroups[?VpcId=='$VPC_ID']|[?GroupName=='$SG_NAME'].[GroupId][0][0]" --output text)
+    SG_ID=$(aws ec2 describe-security-groups --query "SecurityGroups[?VpcId=='$VPC_ID']|[?GroupName=='$SG_ELB_NAME'].[GroupId][0][0]" --output text)
 
     if [ "$SG_ID" == "None" ]
     then
-        echo '>>>' $SG_NAME 'security group missing'
+        echo '>>>' $SG_ELB_NAME 'security group missing'
     else
 
         # http://docs.aws.amazon.com/cli/latest/reference/elb/describe-load-balancers.html
@@ -50,7 +55,7 @@ else
 
             # http://docs.aws.amazon.com/cli/latest/reference/elb/create-load-balancer.html
             # Note keep $VPC_SUBNETS without quotes otherwise the aws cli sees one concatenated value instead of 3 separated with spaces
-            ELB_DNS=$(aws elb create-load-balancer --load-balancer-name "$ELB_NAME" --listeners "$ELB_HTTP_LISTENER" --subnets $VPC_SUBNETS --security-groups "$SG_ID" --query "DNSName" --output text)
+            ELB_DNS=$(aws elb create-load-balancer --load-balancer-name "$ELB_NAME" --listeners "$ELB_HTTP_LISTENER" "$ELB_HTTPS_LISTENER" --subnets $VPC_SUBNETS --security-groups "$SG_ID" --query "DNSName" --output text)
             echo '>>>' $ELB_DNS 'load balancer created'
 
             # TODO Add access logs
